3ca703aa8f74ac340d7eff37a5cf88d6185f62b2
pranali2808 <ranepranali1997@gmail.com>
2022-06-05 20:19:52 +0530

    Event listener on document load

--- JS/EmpPayrollRegistration.js
+++ JS/EmpPayrollRegistration.js
@@ -1,6 +1,25 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function(){
+window.addEventListener('DOMContentLoaded',(event) =>{
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
-})
+    salary.addEventListener('input', function(){
+        output.textContent = salary.value;
+    });
+    
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input',function(){
+      if(name.value.length == 0){
+        textError.textContent = "";
+        return;
+      }
+      try{
+        (new EmployeePayrollData()).name = name.value;
+        textError.textContent = "";
+      }
+      catch(ex){
+        textError.textContent = ex;
+      }
+    }); 
+    });
